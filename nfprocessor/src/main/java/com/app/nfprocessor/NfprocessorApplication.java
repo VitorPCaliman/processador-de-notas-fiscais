53d05a383cb4cad1073468d2c46306967610d680
Vitor P. Caliman <101575225+VitorPCaliman@users.noreply.github.com>
2025-02-07 20:08:43 -0300

    Commit inicial

---
+++ nfprocessor/src/main/java/com/app/nfprocessor/NfprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.app.nfprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfprocessorApplication.class, args);
+	}
+
+}
---
+++ nfprocessor/src/test/java/com/app/nfprocessor/NfprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nfprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
